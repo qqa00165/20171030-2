f6ebb8308cb1a7b04e1a84ec61710a0c068d16e6
qqa00165 <qqa00165@gmail.com>
2017-10-30 13:48:39 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        if(a%2==1);
+        System.out.println("奇");
+        int b = in.nextInt();
+        if(a%2==0);
+        System.out.println("偶");
+    }
+}
